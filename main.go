5eddda24781b47c8f3a1da1b11830835e5036790
ynaits <its@yna.com.au>
2022-01-12 14:06:49 +1030

    Cleanup code

--- main.go
+++ main.go
@@ -1,47 +1,44 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "os"
-  "strings"
-  "github.com/atotto/clipboard"
+	"bufio"
+	"fmt"
+	"github.com/atotto/clipboard"
+	"os"
+	"strings"
 )
 
 func main() {
 
-  reader := bufio.NewReader(os.Stdin)
-  fmt.Println("Case Converter")
-  fmt.Println("=============")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Case Converter")
+	fmt.Println("=============")
 
-  for {
-	 fmt.Print("Enter text to Convert :=> ")
-    text, _ := reader.ReadString('\n')
-    // convert CRLF to LF
-    text = strings.Replace(text, "\n", "", -1)
-fmt.Println(properTitleCase(text))
-  converted:= properCase(text)
-	fmt.Println(converted)
-  clipboard.WriteAll(converted)
-
-  }
+	for {
+		fmt.Print("Enter text to Convert :=> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		fmt.Println(properTitleCase(text))
+		converted := properCase(text)
+		fmt.Println(converted)
+		clipboard.WriteAll(converted)
+	}
 
 }
-
 
 func properCase(input string) string {
-  toLower:=strings.ToLower(input)
-  return properTitleCase(toLower)
+	toLower := strings.ToLower(input)
+	return properTitleCase(toLower)
 }
 
-
 func properTitleCase(input string) string {
-    words := strings.Split(input, " ")
-    smallwords := " a an on the to "
+	words := strings.Split(input, " ")
+	smallwords := " a an on the to "
 
-    for index, word := range words {
-        if strings.Contains(smallwords, " "+word+" ") && word != string(word[0]) {
-            words[index] = word
+	for index, word := range words {
+		if strings.Contains(smallwords, " "+word+" ") && word != string(word[0]) {
+			words[index] = word
 		} else {
 			words[index] = strings.Title(word)
 		}
